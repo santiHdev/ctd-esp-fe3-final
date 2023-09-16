9c299541b32ca4c24fa956fb9b4626cf6b383372
santiç <santihermor50@gmail.com>
2023-09-16 11:48:03 -0300

    cambio de titulo

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -14,8 +14,7 @@ const Favs = () => {
     <div className="favs">
       <h1>Dentists Favs</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+        
         {state.favs.map(fav => <Card name={fav.favName} username={fav.favUsername} key={fav.favId}></Card>)}
       </div>
     </div>
